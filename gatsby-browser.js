256839bc7b15f11dfb7bc584282a52ccff4f1d43
Julieta Campos <jcampos@itexico.net>
2019-10-05 16:25:55 +0200

    re-add code syntax highlight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
@@ -6,3 +7,7 @@
 
 // You can delete this file if you're not using it
 // custom typefaces
+=======
+import 'prism-theme-night-owl'
+import './src/style/prism-overrides.css'
+>>>>>>> 0f6f4c1... re-add code syntax highlight
