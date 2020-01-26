a2662fd4686b003bd7df4a1a518cbca97049d542
Julieta Cristina Campos Guzmán <julieta@Julietas-MacBook-Air.local>
2020-01-26 17:48:32 +0100

    Re-work bio component

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,6 +8,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import "./bio.scss"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -37,28 +38,16 @@ const Bio = () => {
     <div
     className="bio"
     >
+      <div className="bio__frame">
       <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
+        className="bio__image"
       />
-      <p>
+      <p className="bio__text">
         Escrito por <strong>{author}</strong>, una ingeniero en software mexicana que vive en Berlín. Me gusta programar, el maquillaje, la repostería y los ejercicios de ballet.
-        <br /> <br />
-        <a target="_blank" rel="noopener noreferrer" href={`https://twitter.com/${social.twitter}`}>
-          Twitter
-        </a>
-        {` `}|{` `}
-        <a target="_blank" rel="noopener noreferrer" href={`https://instagram.com/${social.instagram}`}>
-          Instagram
-        </a>
-        {` `}|{` `}
-        <a target="_blank" rel="noopener noreferrer" href={`https://github.com/${social.github}`}>
-          GitHub
-        </a>
       </p>
+      </div>
     </div>
   )
 }
