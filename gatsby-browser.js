f3b0323c38b7205ed8806f1b9355abdf1ae93be3
Julieta Cristina Campos Guzmán <julieta@Julietas-MacBook-Air.local>
2020-01-26 17:46:52 +0100

    update project data and dependencies

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
@@ -7,7 +6,3 @@
 
 // You can delete this file if you're not using it
 // custom typefaces
-=======
-import 'prism-theme-night-owl'
-import './src/style/prism-overrides.css'
->>>>>>> 0f6f4c1... re-add code syntax highlight
