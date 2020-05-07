2cb37223cd6b08ab20ad37d66f9856d285db2291
Julieta Campos <julieicelie@gmail.com>
2020-05-07 21:48:10 +0200

    Add plugin for env variables

--- src/services/instagramService.js
+++ src/services/instagramService.js
@@ -1,10 +1,9 @@
 const axios = require("axios")
 
 const BASE_URL = "https://graph.instagram.com/me"
-const ACCESS_TOKEN = process.env.INSTAGRAM_TOKEN
 
 export const getInstagramFeed = () => {
   return axios.get(
-    `${BASE_URL}/media?fields=media_url,caption,permalink&access_token=${ACCESS_TOKEN}`
+    `${BASE_URL}/media?fields=media_url,caption,permalink&access_token=${process.env.INSTAGRAM_TOKEN}`
   )
 }
