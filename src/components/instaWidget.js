c55e56361a9336c43b280eaaa87548ad277e6bd2
Julieta Campos <julieicelie@gmail.com>
2020-05-07 10:57:52 +0200

    Make a pre-loading state for instagram widget

--- src/components/instaWidget.js
+++ src/components/instaWidget.js
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from 'react'
 import './instaWidget.scss'
 import { getInstagramFeed } from '../services/instagramService'
 
+const showPicturePlaceholder = (picture) => {
+  if (!picture) {
+    return <div className="instaPictureFrame emptyFrame"></div>
+  }
+
+  return <div className="instaPictureFrame">
+  <a className="instaPictureLink" href={picture.permalink} target="_blank" rel="noopener noreferrer" key={picture.id} >
+    <img className="instaPicture" src={picture.media_url} alt={picture.caption} />
+  </a>
+</div>
+}
+
 const InstaWidget = () => {
-  const [pictures, setPictures] = useState([]);
+  const [pictures, setPictures] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
 
   useEffect(() => {
     getInstagramFeed().then(data => {
-        const firstTwelvePictures = data.data.data.slice(0, 8)
-        setPictures(firstTwelvePictures)
+        const firstEightPictures = data.data.data.slice(0, 8)
+        setPictures(firstEightPictures)
     })
   }, []);
 
   return (
     <div className="instaPictures">
-      {pictures.map(picture => 
-      <div className="instaPictureFrame">
-        <a className="instaPictureLink" href={picture.permalink} target="_blank" key={picture.id} >
-          <img className="instaPicture" src={picture.media_url} alt={picture.caption} />
-        </a>
-      </div>
-        
-        )}
+      {pictures.map(picture => showPicturePlaceholder(picture))}
     </div>
   );
 
