f2fe65fbe25d38a7f717ca73342519557411265d
Julieta Campos <julieicelie@gmail.com>
2020-05-07 16:40:00 +0200

    Fix styles while the pictures load

--- src/components/instaWidget.js
+++ src/components/instaWidget.js
@@ -1,17 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import './instaWidget.scss'
-import { getInstagramFeed } from '../services/instagramService'
+import React, { useState, useEffect } from "react"
+import "./instaWidget.scss"
+import { getInstagramFeed } from "../services/instagramService"
 
-const showPicturePlaceholder = (picture) => {
+const showPicturePlaceholder = picture => {
   if (!picture) {
-    return <div className="instaPictureFrame emptyFrame"></div>
+    return (
+      <div className="instaPictureFrame">
+        <div className="emptyFrame"></div>
+      </div>
+    )
   }
 
-  return <div className="instaPictureFrame">
-  <a className="instaPictureLink" href={picture.permalink} target="_blank" rel="noopener noreferrer" key={picture.id} >
-    <img className="instaPicture" src={picture.media_url} alt={picture.caption} />
-  </a>
-</div>
+  return (
+    <div className="instaPictureFrame">
+      <a
+        className="instaPictureLink"
+        href={picture.permalink}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={picture.id}
+      >
+        <img
+          className="instaPicture"
+          src={picture.media_url}
+          alt={picture.caption}
+        />
+      </a>
+    </div>
+  )
 }
 
 const InstaWidget = () => {
@@ -24,21 +40,20 @@ const InstaWidget = () => {
     null,
     null,
     null,
-  ]);
+  ])
 
   useEffect(() => {
     getInstagramFeed().then(data => {
-        const firstEightPictures = data.data.data.slice(0, 8)
-        setPictures(firstEightPictures)
+      const firstEightPictures = data.data.data.slice(0, 8)
+      setPictures(firstEightPictures)
     })
-  }, []);
+  }, [])
 
   return (
     <div className="instaPictures">
       {pictures.map(picture => showPicturePlaceholder(picture))}
     </div>
-  );
-
+  )
 }
 
 export default InstaWidget
