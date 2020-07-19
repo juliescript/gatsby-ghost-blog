5ffb454bcd3140468695dc8b1185afefd41fa2bf
Julieta Campos <julieicelie@gmail.com>
2020-07-19 19:01:52 +0200

    Añadir widget de Instagram y Bio

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,18 @@
-import { useStaticQuery, graphql } from "gatsby"
 import React from "react"
+import InstaWidget from "./instaWidget"
+import Bio from "./bio"
 
-const Footer = ({ siteTitle }) => {
-
-  const pageQuery = useStaticQuery(graphql`
-    query {
-      allGhostPage {
-        edges {
-          node {
-            id
-            title
-            slug
-          }
-        }
-      }
-    }
-  `)
-
-  const pages = pageQuery.allGhostPage.edges
+const Footer = () => {
 
   return (
-    <footer className="bg-pink-300 w-screen h-10 py-10 text-gray-100">
+    <footer className="bg-pink-300 w-screen h-full py-10 text-gray-100">
       <section className="container mx-auto">
-        © {new Date().getFullYear()}, Built with
+        <Bio />
+        <h2 className="text-2xl text-white font-serif my-5 border-b-2 border-pink-200">Sígueme en Instagram</h2>
+        <InstaWidget />
+        © {new Date().getFullYear()}, Hecho con
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <a href="https://www.gatsbyjs.org" target="_blank">Gatsby</a> y <a href="https://ghost.org/" target="_blank">Ghost</a>
       </section>
     </footer>
   )
